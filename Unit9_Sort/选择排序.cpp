ba08a30c195729d9bbecc4e21a7414ca45bde086
AstreLee <123646523+AstreLee@users.noreply.github.com>
2023-01-29 11:04:14 +0800

    Update 选择排序.cpp

--- Unit9_Sort/选择排序.cpp
+++ Unit9_Sort/选择排序.cpp
@@ -6,7 +6,7 @@ void SelectSort2(int* arr, int n);
 
 
 int main() {
-    int arr[] = {1, 3, 2, 5, 1, 5, 4, 9090};
+    int arr[] = {1, 3, 2, 5, 1, 5, 4, 9999};
     cout << "before sort" << endl;
     for (int i : arr) {
         cout << i << " ";
